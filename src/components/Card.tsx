1d9cd99b02e81591120f8d2fc1370d63c4c003b0
dat truong <thelegendaglx@gmail.com>
2024-03-17 14:24:28 +0700

    update code

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,31 +1,139 @@
-import { useState } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 
-function Card({
-  front,
-  back,
-  onClick,
-  onDbClick,
-}: {
-  front: string;
-  back: string;
-  onClick?: () => void;
-  onDbClick?: () => void;
-}) {
-  const [isFront, setIsFront] = useState(true);
-
-  const handleClick = () => {
-    setIsFront((prev) => !prev);
-  };
-
-  const Leaf = (text: string) => {
-    return (
-      <div className="bg-slate-300 w-full h-[200px] mb-3 flex items-center justify-center border-solid border-1 rounded-lg overflow-hidden">
-        {text}
-      </div>
-    );
-  };
-
-  return <div onClick={handleClick}>{isFront ? Leaf(front) : Leaf(back)}</div>;
+export interface CardRef {
+  getCards: () => Card[];
 }
 
-export default Card;
+export interface Card {
+  sentence: string;
+  meaning: string;
+  color: string;
+  isHidden: boolean;
+  isFront: boolean;
+}
+
+function mappingCard(item: any, index: number) {
+  const color = index % 2 ? "rgb(226 232 240)" : "rgb(203 213 225)";
+  return {
+    sentence: item.sentence,
+    meaning: item.meaning,
+    color: color,
+    isHidden: false,
+    isFront: false,
+  };
+}
+
+const random = (list: Card[]) => {
+  let index = list.length;
+
+  while (index > 0) {
+    const newIndex = Math.floor(Math.random() * list.length);
+    index--;
+
+    const temp = list[index];
+    list[index] = list[newIndex];
+    list[newIndex] = temp;
+  }
+
+  return list;
+};
+
+const CardList = forwardRef<CardRef, { data: any[] }>(({ data }, ref) => {
+  const [cards, setCards] = useState<Card[]>([]);
+  useImperativeHandle(
+    ref,
+    () => ({
+      getCards: () => {
+        return cards;
+      },
+    }),
+    [cards]
+  );
+
+  useEffect(() => {
+    setCards(random(data.map(mappingCard)));
+  }, [data]);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>, card: Card) => {
+    // double click
+    if (event.detail === 2) {
+      handleDbClick(card);
+      return;
+    }
+
+    // click
+    card.isFront = !card.isFront;
+    setCards([...cards]);
+  };
+
+  const handleDbClick = (card: Card) => {
+    card.isHidden = true;
+    setCards([...cards]);
+  };
+
+  return (
+    <ul className="mt-4 flex gap-2 flex-wrap flex-col md:flex-row">
+      {cards.map((card) => (
+        <li key={card.sentence} className={card.isHidden ? "hidden" : ""}>
+          <div
+            onClick={(event) => handleClick(event, card)}
+            className="w-full md:w-[336px] h-[200px] mb-3 p-4 flex items-center justify-center border-solid border-1 rounded-lg overflow-hidden cursor-pointer"
+            style={{ backgroundColor: card.color }}
+          >
+            {card.isFront ? (
+              <div>{card.sentence}</div>
+            ) : (
+              <div>{card.meaning}</div>
+            )}
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+});
+// function Card({ data }: { data: any[] }) {
+//   const [cards, setCards] = useState<Card[]>([]);
+
+//   useEffect(() => {
+//     setCards(random(data.map(mappingCard)));
+//   }, [data]);
+
+//   const handleClick = (event: React.MouseEvent<HTMLDivElement>, card: Card) => {
+//     // double click
+//     if (event.detail === 2) {
+//       handleDbClick(card);
+//       return;
+//     }
+
+//     // click
+//     card.isFront = !card.isFront;
+//     setCards([...cards]);
+//   };
+
+//   const handleDbClick = (card: Card) => {
+//     card.isHidden = true;
+//     setCards([...cards]);
+//   };
+
+//   return (
+//     <ul className="mt-4 flex gap-2 flex-wrap flex-col md:flex-row ">
+//       {cards.map((card) => (
+//         <li className={card.isHidden ? "hidden" : ""}>
+//           <div
+//             onClick={(event) => handleClick(event, card)}
+//             className="w-full md:w-[336px] h-[200px] mb-3 flex items-center justify-center border-solid border-1 rounded-lg overflow-hidden cursor-pointer"
+//             style={{ backgroundColor: card.color }}
+//           >
+//             {card.isFront ? (
+//               <div>{card.sentence}</div>
+//             ) : (
+//               <div>{card.meaning}</div>
+//             )}
+//           </div>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+export default CardList;
--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -1,9 +1,9 @@
 import { useLoaderData } from "react-router-dom";
 import { Group, Sentence } from "./group";
-import Card from "./Card";
+import CardList, { CardRef, Card } from "./Card";
 import Button from "./Button";
 import Anchor from "./Anchor";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { setGroupByName } from "../common";
 
 interface Flashcard {
@@ -16,116 +16,48 @@ interface Flashcard {
 }
 // 0 => 1, 1 => 2, 2 => 2
 const PASSED = [1, 2, 2];
-let timeOutClick: any = null;
-let click = 0;
-
-const random = (list: unknown[]) => {
-  let index = list.length;
-
-  while (index > 0) {
-    const newIndex = Math.floor(Math.random() * list.length);
-    index--;
-
-    const temp = list[index];
-    list[index] = list[newIndex];
-    list[newIndex] = temp;
-  }
-
-  return list;
-};
 
 function Flashcard() {
   const { name, sentences: data } = useLoaderData() as Group;
   const [state, setState] = useState(0);
-  const [flashcards, setFlashcards] = useState<Flashcard[]>([]);
+  const [flashcards, setFlashcards] = useState<any[]>([]);
+  const cardRef = useRef<CardRef | null>(null);
 
   useEffect(() => {
-    const flashcards = data.map((flashcard, index) => {
-      const color = index % 2 ? "rgb(226 232 240)" : "rgb(203 213 225)";
-      return {
-        sentence: flashcard.sentence,
-        meaning: flashcard.meaning,
-        color: color,
-        isFront: false,
-        state: flashcard.state === undefined ? 0 : flashcard.state,
-        isHidden: false,
-      };
-    });
-    setFlashcards((prev) => [...flashcards]);
+    setFlashcards([
+      ...data.filter((s) => s.state === undefined || s.state === state),
+    ]);
   }, []);
 
-  useEffect(() => {
-    save();
-  }, [state]);
-
-  const handleClick = (flashcard: Flashcard) => {
-    click += 1;
-
-    if (!timeOutClick) {
-      timeOutClick = setTimeout(() => {
-        const fc = flashcards.find((d) => d.sentence === flashcard.sentence);
-        if (!fc) {
-          return false;
-        }
-        fc.isFront = !fc.isFront;
-        setFlashcards([...flashcards]);
-
-        clearTimeout(timeOutClick);
-        timeOutClick = null;
-        click = 0;
-      }, 200);
-    }
-
-    if (click === 2) {
-      clearTimeout(timeOutClick);
-      timeOutClick = null;
-      click = 0;
-
-      handleDbClick(flashcard);
-    }
-  };
-
-  const handleDbClick = (flashcard: Flashcard) => {
-    const fc = flashcards.find((d) => d.sentence === flashcard.sentence);
-    if (!fc) {
-      return false;
-    }
-    const newState = PASSED[fc.state];
-    fc.state = newState;
-    fc.isHidden = true;
-
-    setFlashcards([...flashcards]);
-  };
-
   const handleNextState = () => {
-    const failed = flashcards.map((flashcard) => {
-      if (flashcard.state === state && !flashcard.isHidden) {
-        flashcard.state = 0;
-      }
-      flashcard.isHidden = false;
-      flashcard.isFront = false;
-      return flashcard;
-    });
+    const newState = (state + 1) % 3;
 
-    //const shuffle = random(failed)
-
-    setFlashcards((prev) => [...failed]);
-    setState((prev) => (prev + 1) % 3);
+    setState(newState);
+    let cards = cardRef?.current?.getCards() || [];
+    // save data to local
+    const newData = saveState(cards, data);
+    setGroupByName(name, newData);
+    setFlashcards(newData.filter((item) => item.state === newState));
   };
-  const save = () => {
-    const sentences = data.map((sentence) => {
-      const flashcard = flashcards.find(
-        (fc) => fc.sentence === sentence.sentence
-      );
-      if (!flashcard) {
-        return sentence;
+
+  const saveState = (cards: Card[], sentences: Sentence[]) => {
+    if (cards.length === 0) {
+      return sentences;
+    }
+    const newSentences = sentences.map((item) => {
+      const card = cards.find((c) => c.sentence === item.sentence);
+      if (!card) {
+        return item;
       }
-      return {
-        ...sentence,
-        state: flashcard.state,
-      };
+
+      let state = item.state || 0;
+      state = card.isHidden ? PASSED[state] : 0;
+      item.state = state;
+      return item;
     });
-    setGroupByName(name, sentences);
+    setGroupByName(name, newSentences);
+
+    return newSentences;
   };
 
   return (
@@ -136,23 +68,7 @@ function Flashcard() {
       </h1>
       <Button name={`Next (${state + 1})`} onClick={handleNextState} />
       <ul className="mt-4 flex gap-2 flex-wrap flex-col md:flex-row ">
-        {flashcards
-          .filter((d) => d.state === state)
-          .map((flashcard) => (
-            <li className={flashcard.isHidden ? "hidden" : ""}>
-              <div
-                onClick={() => handleClick(flashcard)}
-                className="w-full md:w-[336px] h-[200px] mb-3 flex items-center justify-center border-solid border-1 rounded-lg overflow-hidden cursor-pointer"
-                style={{ backgroundColor: flashcard.color }}
-              >
-                {flashcard.isFront ? (
-                  <div>{flashcard.sentence}</div>
-                ) : (
-                  <div>{flashcard.meaning}</div>
-                )}
-              </div>
-            </li>
-          ))}
+        <CardList ref={cardRef} data={flashcards} />
       </ul>
     </section>
   );
